8a9fd697096ae1972dc98973c43da2bd2be482f3
JoshuaToback <joshuatoback@gmail.com>
2023-09-03 21:11:57 -0400

    Moved dani again

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ function App() {
           <Route path="/faq" element={<Faq />}></Route>
           <Route path="/dropoff" element={<Dropoff />}></Route>
           <Route path="/autograph" element={<Autograph />}></Route>
-          <Route path ="/danielletoback" element={<DanielleToback />}></Route>
         </Routes>
         <Footer />
       </Container>
